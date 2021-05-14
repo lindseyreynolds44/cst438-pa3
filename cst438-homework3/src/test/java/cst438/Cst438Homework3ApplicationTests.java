64f9e2f17feea3f12a979b30f2c496341e3e2043
lindseyreynolds44 <56812063+lindseyreynolds44@users.noreply.github.com>
2021-05-14 12:43:45 -0700

    Initial commit

---
+++ cst438-homework3/src/test/java/cst438/Cst438Homework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Homework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
